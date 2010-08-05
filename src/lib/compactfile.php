e27eefd061746a471124a4d4174e766c38a8ef9d
Jurriaan Pruis <jurriaan@colouredpeople.com>
2010-08-06 01:20:48 +0800

    Added some more documentation Cleaned compactfile.php Moved all output to compactor.php (report())

--- src/lib/compactfile.php
+++ src/lib/compactfile.php
@@ -8,53 +8,55 @@
  **/
  
 class CompactFile {
-  private $filename,$out;
-  public $filesize,$compressedsize = 0;
+  private $out;
+  public $filesize, $compressedsize, $filename;
+  
+  /**
+   * Creates CompactFile object
+   *
+   * @param string $filename
+   *  The input file.
+   * @param resource $out
+   *  The output filepointer to which the compressed data is appended.
+   */
   public function __construct($filename,$out) {
     $this->filename = $filename;
     $this->filesize = filesize($filename);
     $this->out = $out;
   }
+  
+  /**
+   * Compacts $filename.
+   */
   public function compact() {
-    printf("Compacting %s -- filesize: %dB", basename($this->filename), $this->filesize);
     $tokens = $this->getTokens();
-    $next = false;
-    $start = ftell($this->out);
+    $removenext = false; // Remove next whitespace
+    $start = ftell($this->out); // Position in output
     $len = count($tokens);
     for ($i=0;$len > $i;$i++) {
       $token = $tokens[$i];
-			if($i+1 < $len) {
-			  $nexttoken = $tokens[$i+1];
-			} else {
-			  $nexttoken = '';
-			}
-			if($i-1 > -1) {
-			  $prevtoken = $tokens[$i-1];
-			} else {
-			  $prevtoken = '';
-			}
-			if (is_string($token)) {
-	      $this->write($token);
-	      if(in_array($token,Compactor::$safechar)) $next = true;
-	    } else if($token[0] == T_WHITESPACE) {
-	      
-	      if(!$next) {
-	        if(is_string($nexttoken)) {
+      $nexttoken = ($i+1 < $len)?$tokens[$i+1]:'';
+      $prevtoken = ($i-1 > -1)?$tokens[$i-1]:'';
+
+      if (is_string($token)) {
+        if(in_array($token,Compactor::$safechar)) $removenext = true;
+        $this->write($token);
+      } else if($token[0] == T_WHITESPACE) {
+        if(!$removenext) {
+          if(is_string($nexttoken)) {
             if(!in_array($nexttoken,Compactor::$safechar) && !(in_array($nexttoken,Compactor::$semisafe) && in_array($prevtoken[0],Compactor::$keyword))) {
               $this->write(' ');
             }
           } else if(!in_array($nexttoken[0],Compactor::$beforetoken)) {
             $this->write(' ');
           } 
-          
         }
       } else if(in_array($token[0],Compactor::$aftertoken)) {
-        $next = true;
+        $removenext = true;
         $this->write($token[1]);
       } else if(in_array($token[0],Compactor::$removable)) {
-        $next = true;
-      } else if(in_array($token[0],Compactor::$requires)) {
-
+        $removenext = true;
+      } else if(in_array($token[0],Compactor::$requires)) { // Remove require + everything until ';', maybe not safe?
         for ($i2 = $i;$len > $i2;$i2++) {
           $rtoken = &$tokens[$i2];
           if($rtoken == ';') {
@@ -65,17 +67,17 @@ class CompactFile {
           }
         }
       } else {
-        $next = false;
-        
+        $removenext = false;
         $this->write($token[1]);
       }
     }
     $this->compressedsize = ftell($this->out) - $start;
-    printf(", compressed size: %dB, %.2f%%\n",$this->compressedsize,(($this->compressedsize/$this->filesize) - 1)*100);
   }
+  
   private function write($string) {
     fwrite($this->out, $string);
   }
+  
   private function getTokens() {
     return token_get_all(trim(file_get_contents($this->filename)));
   }
